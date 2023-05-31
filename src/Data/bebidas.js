ff4e8472c444bb048dd8644eba394b0f1ee439d8
andrephelipe <andre.phelipe.1999@gmail.com>
2023-05-31 01:04:57 -0300

    arrumando as bebidas add fanta e h2o limoneto

--- src/Data/bebidas.js
+++ src/Data/bebidas.js
@@ -6,6 +6,9 @@ import cocaZero2l from '../../Assets/cocazero2l.jpg';
 import guarana2l from '../../Assets/guarana-2l.jpg';
 import h20 from '../../Assets/h20.png';
 import agua from '../../Assets/agua.png';
+import fantalaranja from '../../Assets/fantalaranja.png';
+import fantauva from '../../Assets/fantauva.jpg';
+import h20limoneto from '../../Assets/h20limoneto.jpg';
 
 const bebidas = [
   {
@@ -31,34 +34,55 @@ const bebidas = [
   },
   {
     id: 4,
+    name: 'Fanta uva lata',
+    image: fantauva,
+    description: '',
+    price: 5.00,
+  },
+  {
+    id: 5,
+    name: 'Fanta laranja lata',
+    image: fantalaranja,
+    description: '',
+    price: 5.00,
+  },
+  {
+    id: 6,
     name: 'Coca-Cola 2l',
     image: coca2l,
     description: '',
     price: 14.00,
   },
   {
-    id: 5,
+    id: 7,
     name: 'Coca-Cola zero 2l',
     image: cocaZero2l,
     description: '',
     price: 14.00,
   },
   {
-    id: 6,
+    id: 8,
     name: 'Guaraná 2l',
     image: guarana2l,
     description: '',
     price: 12.00,
   },
   {
-    id: 7,
-    name: 'H2o 1.5l',
+    id: 9,
+    name: 'H2o 500ml',
     image: h20,
     description: '',
-    price: 12.00,
+    price: 6.00,
   },
   {
-    id: 8,
+    id: 10,
+    name: 'h2o limoneto 500ml',
+    image: h20limoneto,
+    description: '',
+    price: 6.00,
+  },
+  {
+    id: 11,
     name: 'Água 500ml',
     image: agua,
     description: '',
